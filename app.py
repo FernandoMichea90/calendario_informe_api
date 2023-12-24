afbb20646a24f1f47aaab5b0bd8da99bb9aa70ea
Fernando Michea <fmicheam@gmail.com>
2023-12-24 11:32:38 -0300

    se inicia el proyecto

---
+++ app.py
@@ -0,0 +1,28 @@
+from flask import Flask, jsonify
+from flasgger import Swagger
+
+
+app = Flask(__name__)
+swagger = Swagger(app)
+
+@app.route('/hello')
+def hello_world():
+     """
+    Ejemplo de endpoint que saluda al usuario.
+    ---
+    tags:
+      - Saludo
+    responses:
+      200:
+        description: Saludo exitoso
+        schema:
+          properties:
+            message:
+              type: string
+              description: Saludo al usuario
+    """
+    
+     return 'Hello, World!'
+
+if __name__ == '__main__':
+    app.run(debug=True)
